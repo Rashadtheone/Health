c3cb276de51911c60da332d64c616c99e48c3a63
Auxxk Vii <auxxkvii@Rashads-Air.fios-router.home>
2018-11-27 17:28:41 -0500

    changed modal to materalize.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Card,CardTitle,CardText,Input,Button, Modal, ModalHeader, ModalBody, ModalFooter }
- from 'reactstrap';
- import { NavItem, Navbar } from 'react-materialize'
+import { Card,CardTitle,CardText,Input,Button} from 'reactstrap';
+ import { NavItem, Navbar,Modal } from 'react-materialize'
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +9,6 @@ class App extends Component {
 
     this.state = {
       data:null,
-      isOpen: false,
       query: [],
       modal: false
     }
@@ -32,7 +30,7 @@ class App extends Component {
 
   toggle() {
     this.setState({
-      modal: !this.state.modal
+      modal: !this.state.modal,
     });
   }
   
@@ -61,21 +59,12 @@ class App extends Component {
             <CardTitle>{item.title}</CardTitle>
             <CardText>{item.bite}</CardText>
             
-            <Button onClick={this.toggle}>{this.props.buttonLabel}More Info</Button>
-            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-            <ModalHeader toggle={this.toggle} close={closeBtn}>Modal title</ModalHeader>
-            <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-            </ModalBody>
-          < ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+            <Modal
+  header='Modal Header'
+  fixedFooter
+  trigger={<Button>More Info</Button>}>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+</Modal>
           </Card>
             )
           :

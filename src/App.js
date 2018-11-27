c037efd35cf713b5ab07956064a7921167f11a80
Auxxk Vii <auxxkvii@Rashads-Air.fios-router.home>
2018-11-27 17:37:12 -0500

    redesign

--- src/App.js
+++ src/App.js
@@ -57,13 +57,12 @@ class App extends Component {
           
           <Card body>
             <CardTitle>{item.title}</CardTitle>
-            <CardText>{item.bite}</CardText>
             
             <Modal
-  header='Modal Header'
+  header={item.title}
   fixedFooter
   trigger={<Button>More Info</Button>}>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              <p>{item.bite}</p>
 </Modal>
           </Card>
             )
